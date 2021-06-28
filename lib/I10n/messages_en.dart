c5f119939f743269aef75cc120a340f8b25ef4e8
walaaAlshobaki <walaaalshobaki956@gmail.com>
2021-06-28 07:39:27 +0300

    tesف22

--- lib/I10n/messages_en.dart
+++ lib/I10n/messages_en.dart
@@ -19,43 +19,47 @@ class MessageLookup extends MessageLookupByLibrary {
   get localeName => 'en';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "btnsubmit" : MessageLookupByLibrary.simpleMessage("Submit"),
-    "lblemail" : MessageLookupByLibrary.simpleMessage("Email"),
-    "lblname" : MessageLookupByLibrary.simpleMessage("Name"),
-    "lblphone" : MessageLookupByLibrary.simpleMessage("Phone"),
-    "lbllocale" : MessageLookupByLibrary.simpleMessage("en"),
-    "lbltitle" : MessageLookupByLibrary.simpleMessage("Contact Us"),
-     "lblcreatAcount" : MessageLookupByLibrary.simpleMessage("Creat Account"),
-     "lblconfirmPassword" : MessageLookupByLibrary.simpleMessage("Confirm Password"),
-     "lblsignIn" : MessageLookupByLibrary.simpleMessage("Sign In"),
-     "lblbookCourse" : MessageLookupByLibrary.simpleMessage("Book a Course"),
-    "lblpassword" : MessageLookupByLibrary.simpleMessage("Password"),
-    "lblsettings" : MessageLookupByLibrary.simpleMessage("Settings"),
-    "lblchangeLanguage" : MessageLookupByLibrary.simpleMessage("Change Language"),
-    "lblchangePassword" : MessageLookupByLibrary.simpleMessage("Change Password"),
-    "lblsignOut" : MessageLookupByLibrary.simpleMessage("Sign Out"),
-    "lblprofile" : MessageLookupByLibrary.simpleMessage("Profile"),
-  "lblsearch" : MessageLookupByLibrary.simpleMessage("Search"),
-"lblteachercourse" : MessageLookupByLibrary.simpleMessage("Teacher Courses"),
-"lblcheckloginemail" : MessageLookupByLibrary.simpleMessage("Please Write Correct Email and Correct Password"),
-
-
-"chemail" : MessageLookupByLibrary.simpleMessage("Please Write a Correct Email Address "),
-   "chpassword" : MessageLookupByLibrary.simpleMessage(" Please Write a Correct Password "),
-    "chconpassword" : MessageLookupByLibrary.simpleMessage(" Please Write a Correct Confirem Password    "),
-     "chphone" : MessageLookupByLibrary.simpleMessage(" Please Write a Correct Phone Number   "),
-
-
-
-
-
-
-        "lblgooglemap" : MessageLookupByLibrary.simpleMessage("Google Map"),
-        "lblsearchschools" : MessageLookupByLibrary.simpleMessage("   Search For Schools  "),
-        "lblschools" : MessageLookupByLibrary.simpleMessage("   Schools"),
-            "lblmycourse" : MessageLookupByLibrary.simpleMessage("My Course"),
-        "lblcurrentlocation" : MessageLookupByLibrary.simpleMessage("Current Location "),
-     "lblyoucanchangelangugejustclickbutton" : MessageLookupByLibrary.simpleMessage("You Can Change Languge Just Click Button"),
-  };
+  static _notInlinedMessages(_) => <String, Function>{
+        "btnsubmit": MessageLookupByLibrary.simpleMessage("Submit"),
+        "lblemail": MessageLookupByLibrary.simpleMessage("Email"),
+        "lblname": MessageLookupByLibrary.simpleMessage("Name"),
+        "lblphone": MessageLookupByLibrary.simpleMessage("Phone"),
+        "lbllocale": MessageLookupByLibrary.simpleMessage("en"),
+        "lbltitle": MessageLookupByLibrary.simpleMessage("Contact Us"),
+        "lblcreatAcount": MessageLookupByLibrary.simpleMessage("Creat Account"),
+        "lblconfirmPassword":
+            MessageLookupByLibrary.simpleMessage("Confirm Password"),
+        "lblsignIn": MessageLookupByLibrary.simpleMessage("Sign In"),
+        "lblbookCourse": MessageLookupByLibrary.simpleMessage("Book a Course"),
+        "lblpassword": MessageLookupByLibrary.simpleMessage("Password"),
+        "lblsettings": MessageLookupByLibrary.simpleMessage("Settings"),
+        "lblchangeLanguage":
+            MessageLookupByLibrary.simpleMessage("Change Language"),
+        "lblchangePassword":
+            MessageLookupByLibrary.simpleMessage("Change Password"),
+        "lblsignOut": MessageLookupByLibrary.simpleMessage("Sign Out"),
+        "lblprofile": MessageLookupByLibrary.simpleMessage("Profile"),
+        "lblsearch": MessageLookupByLibrary.simpleMessage("Search"),
+        "lblteachercourse":
+            MessageLookupByLibrary.simpleMessage("Teacher Courses"),
+        "lblcheckloginemail": MessageLookupByLibrary.simpleMessage(
+            "Please Write Correct Email and Correct Password"),
+        "chemail": MessageLookupByLibrary.simpleMessage(
+            "Please Write a Correct Email Address "),
+        "chpassword": MessageLookupByLibrary.simpleMessage(
+            " Please Write a Correct Password "),
+        "chconpassword": MessageLookupByLibrary.simpleMessage(
+            " Please Write a Correct Confirem Password    "),
+        "chphone": MessageLookupByLibrary.simpleMessage(
+            " Please Write a Correct Phone Number   "),
+        "lblgooglemap": MessageLookupByLibrary.simpleMessage("Google Map"),
+        "lblsearchschools":
+            MessageLookupByLibrary.simpleMessage("   Search For Schools  "),
+        "lblschools": MessageLookupByLibrary.simpleMessage("   Schools"),
+        "lblcurrentlocation":
+            MessageLookupByLibrary.simpleMessage("Current Location "),
+        "lblyoucanchangelangugejustclickbutton":
+            MessageLookupByLibrary.simpleMessage(
+                "You Can Change Languge Just Click Button"),
+      };
 }
