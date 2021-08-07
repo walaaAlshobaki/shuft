e37df2131fd549ad27e9282b497d80a2ced1bf23
walaaAlshobaki <walaaalshobaki956@gmail.com>
2021-08-07 04:10:11 +0300

    yyyyy

--- lib/select_your_region.dart
+++ lib/select_your_region.dart
@@ -4,7 +4,7 @@ import 'package:shaftt_app/themes.dart';
 import 'package:shaftt_app/your_class_when_you_want.dart';
 
 class SelectYourRegion extends StatefulWidget {
- @override
+  @override
   _SelectYourRegionState createState() => _SelectYourRegionState();
 }
 
@@ -13,65 +13,57 @@ class _SelectYourRegionState extends State<SelectYourRegion> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-       child: Stack(
-         children: <Widget>[
-           
+        child: Stack(
+          children: <Widget>[
             Padding(
-          padding: EdgeInsets.fromLTRB(360, 60, 0, 22),
-          child: Text('Skip',
-            style: TextStyle(
-              fontSize: 19.0,
-                    fontFamily:"HelveticaBold",
-                    color: new Color(0xff55CE9D),
-                   
-              )
-              )
+                padding: EdgeInsets.fromLTRB(360, 60, 0, 22),
+                child: Text('Skip',
+                    style: TextStyle(
+                      fontSize: 19.0,
+                      fontFamily: "HelveticaBold",
+                      color: new Color(0xff55CE9D),
+                    ))),
+            Padding(
+                padding: EdgeInsets.fromLTRB(33, 100, 33, 0),
+                child: Text('Select your region',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                      fontFamily: "HelveticaBold",
+                      color: new Color(0xff55CE9D),
+                    ))),
+            Center(
+              child: Image.asset(
+                "assets/img/g3.png",
+                width: 277,
+                height: 195,
               ),
-         Padding(
-          padding: EdgeInsets.fromLTRB(33, 100, 33, 0),
-          child: Text('Select your region',
-            style: TextStyle(
-              fontSize: 35.0,
-                    fontFamily:"HelveticaBold",
-                    color: new Color(0xff55CE9D),
-                   
-              )
-              )
-              ),
-               Center(child: Image.asset("assets/img/g3.png",width: 277,height: 195,) ,),
-               Padding(
-          padding: EdgeInsets.fromLTRB(20, 650,10, 0),
-          child: Text('Choose the Area you want or you in.',
-            style: TextStyle(
-              fontSize: 20.0,
-                    fontFamily:"HelveticaBold",
-                    color: new Color(0xff707070),
-                   
-              )
-              )
-              ),
-              Padding(
-          padding: EdgeInsets.fromLTRB(260, 750,10, 0),
-          child:  roundedRectButton("Next",Colors.white, blueGradients, LetsStarting()),
-                 
-              ),
-               Padding(
-          padding: EdgeInsets.fromLTRB(20, 750,0, 0),
-          child:  roundedRectButton("Back",Color(0xff55CE9D), whiteGradients, YourLessonsWhenYouWant()),
-                 
-              ),
-              
-         ],
-       ),
-        
-       
-       
+            ),
+            Padding(
+                padding: EdgeInsets.fromLTRB(20, 650, 10, 0),
+                child: Text('Choose the Area you want or you in.',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: "HelveticaBold",
+                      color: new Color(0xff707070),
+                    ))),
+            Padding(
+              padding: EdgeInsets.fromLTRB(260, 750, 10, 0),
+              child: roundedRectButton(
+                  "Next", Colors.white, blueGradients, LetsStarting()),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 750, 0, 0),
+              child: roundedRectButton("Back", Color(0xff55CE9D),
+                  whiteGradients, YourLessonsWhenYouWant()),
+            ),
+          ],
+        ),
       ),
-      
     );
-    
   }
-   Widget roundedRectButton(String title,Color textColor, List<Color> gradient, Widget page) {
+
+  Widget roundedRectButton(
+      String title, Color textColor, List<Color> gradient, Widget page) {
     return Builder(builder: (BuildContext mContext) {
       return Padding(
         padding: EdgeInsets.only(bottom: 10),
@@ -95,7 +87,7 @@ class _SelectYourRegionState extends State<SelectYourRegion> {
                     style: TextStyle(
                         color: textColor,
                         fontSize: 22,
-                        fontFamily:"HelveticaMedium",
+                        fontFamily: "HelveticaMedium",
                         fontWeight: FontWeight.w300)),
               ),
               onTap: () {
